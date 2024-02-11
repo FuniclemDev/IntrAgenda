f42ba777ea31bc7995d3ce400e990eca08d38f54
Clement DORGE <clement.dorge@epitech.eu>
2024-02-11 02:23:09 +0100

    [+] Install python3.12-devel

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -2,6 +2,7 @@
 
 # For Fedora 38
 sudo dnf install python3
+sudo dnf install python3.12-devel
 sudo dnf install python3-pip
 sudo dnf install chromedriver-120.0.6099.71-1.fc38.x86_64
-pip install google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client selenium cryptography cx_Freeze
+pip install google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client selenium pyinstaller cryptography cx_Freeze
